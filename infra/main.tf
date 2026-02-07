a882eac1c65bdef57711150a0d1e07e654dcb8c5
Llorenç <llorensmr@gmail.com>
2026-02-07 22:41:56 +0000

    [Hotfix] Proper path for services build step

--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "cloud-demo-terraform"  # replace with your bucket name
     key            = "terraform.tfstate"            # path inside bucket
     region         = "eu-west-1"                    # AWS region
-    dynamodb_table = "terraform-locks"              # optional for state locking
+    dynamodb_table = "use_lockfile"              # optional for state locking
     encrypt        = true                            # encrypt state at rest
   }
 }
