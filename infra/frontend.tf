dcf466baf03ff08a563178bb2973dd6584ce79b4
Llorenç <llorensmr@gmail.com>
2026-02-08 08:56:23 +0000

    Properly set front bucket

--- infra/frontend.tf
+++ infra/frontend.tf
@@ -1,7 +1,32 @@
 # S3 bucket for frontend
 resource "aws_s3_bucket" "frontend" {
   bucket = "${var.project_name}-frontend"
-  aws_s3_bucket_acl    = "public-read"
+}
+
+resource "aws_s3_bucket_ownership_controls" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "frontend" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.frontend,
+    aws_s3_bucket_public_access_block.frontend,
+  ]
+
+  bucket = aws_s3_bucket.frontend.id
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_website_configuration" "frontend_website" {
