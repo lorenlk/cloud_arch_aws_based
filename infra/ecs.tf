deb38b1497654f5fc936533c90cd25fea1782863
Llorenç <llorensmr@gmail.com>
2026-02-08 14:48:00 +0000

    Use wget for health check to avoid tasks being marked as unhealthy

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -40,11 +40,11 @@ resource "aws_ecs_task_definition" "tasks" {
         }
       ]
       healthCheck = {
-        command     = ["CMD-SHELL", "curl -f http://localhost:${each.value.port}/health || exit 1"]
+        command     = ["CMD-SHELL", "wget --no-verbose --tries=1 --spider http://localhost:${each.value.port}/health || exit 1"]
         interval    = 30
         timeout     = 5
         retries     = 3
-        startPeriod = 10
+        startPeriod = 30
       }
 
       logConfiguration = {
