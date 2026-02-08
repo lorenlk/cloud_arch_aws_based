52d6a1c0e08da68bb06701e53c0627c6b4205181
Llorenç <llorensmr@gmail.com>
2026-02-08 11:36:37 +0000

    Renamed attribute

--- infra/variables.tf
+++ infra/variables.tf
@@ -55,14 +55,14 @@ variable "services" {
     port        = number
     expose      = bool
     path_prefix = string
-    endpoint_path = list(string)
+    endpoints = list(string)
   }))
   default = {
-    service_a = { port = 8000, expose = true, path_prefix = "/api", endpoint_path = ["/calculate"]}
-    service_b = { port = 8001, expose = true, path_prefix = "/api", endpoint_path = ["/add"]}
-    service_c = { port = 8002, expose = false, path_prefix = "", endpoint_path = [] }
-    service_d = { port = 8003, expose = false, path_prefix = "", endpoint_path = [] }
-    service_e = { port = 8004, expose = true, path_prefix = "/api", endpoint_path = ["/count_double"]}
-    service_f = { port = 8005, expose = false, path_prefix = "", endpoint_path = [] }
+    service_a = { port = 8000, expose = true, path_prefix = "/api", endpoints = ["/calculate"]}
+    service_b = { port = 8001, expose = true, path_prefix = "/api", endpoints = ["/add"]}
+    service_c = { port = 8002, expose = false, path_prefix = "", endpoints = [] }
+    service_d = { port = 8003, expose = false, path_prefix = "", endpoints = [] }
+    service_e = { port = 8004, expose = true, path_prefix = "/api", endpoints = ["/count_double"]}
+    service_f = { port = 8005, expose = false, path_prefix = "", endpoints = [] }
   }
 }
